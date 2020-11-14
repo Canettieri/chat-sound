1682e634c2de78fd388d509707d2077ba6e02934
Eliote <>
2020-11-14 14:42:14 -0300

    Add some new sounds

--- SoundsRegistry.lua
+++ SoundsRegistry.lua
@@ -1,7 +1,7 @@
 local ADDON_NAME, L = ...
 local LSM = LibStub("LibSharedMedia-3.0")
 
-for i = 1, 8 do
+for i = 1, 12 do
 	LSM:Register(
 			LSM.MediaType.SOUND, "CSC Sound " .. i,
 			"Interface\\AddOns\\" .. ADDON_NAME .. "\\audios\\sound" .. i .. ".mp3"
